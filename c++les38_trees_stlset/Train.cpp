f6ac7ceb58b92922bbf93d2895e94f2c33ddacde
KhGnome <mihail.tsymbal2000@gmail.com>
2025-06-24 18:15:11 +0300

    yo

--- c++les38_trees_stlset/Train.cpp
+++ c++les38_trees_stlset/Train.cpp
@@ -1,1 +1,50 @@
 #include "Train.h"
+
+Train::Train()
+{
+	number = "";
+	timedeparture = "";
+	station = "";
+}
+
+Train::Train(string number, string timedeparture, string station)
+{
+	this->number = number;
+	this->timedeparture = timedeparture;
+	this->station = station;
+}
+
+void Train::setNumber(string number)
+{
+	this->number = number;
+}
+
+void Train::setTimedaparture(string timedeparture)
+{
+	this->timedeparture = timedeparture;
+}
+
+void Train::setStation(string station)
+{
+	this->station = station;
+}
+
+string Train::getNumber() const
+{
+	return number;
+}
+
+string Train::getTimedaparture() const
+{
+	return timedeparture;
+}
+
+string Train::getStation() const
+{
+	return station;
+}
+
+void Train::showInfo() const
+{
+	cout << timedeparture << " " << number << " " << station << endl;
+}
